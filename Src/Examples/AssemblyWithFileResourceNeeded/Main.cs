51a2c7f04d8c8bdc9d8de49ab276aa544a0a2207
Antonio Parata <aparata@gmail.com>
2018-12-20 16:00:53 +0100

    Added example on external file needed

---
+++ Src/Examples/AssemblyWithFileResourceNeeded/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyWithFileResourceNeeded
+{
+    public class Main
+    {
+        // this is the standard signature for activating the Assembly
+        private static void Inject()
+        {
+            File.AppendAllText("log_AssemblyWithFileResourceNeeded.txt", "Correctly activated");
+            var fileContent = File.ReadAllText("my_file.txt");
+            File.AppendAllText("log_AssemblyWithFileResourceNeeded.txt", "\r\nFile content: " + fileContent);
+        }
+    }
+}
--- Src/Examples/TestRunner/Program.cs
+++ Src/Examples/TestRunner/Program.cs
@@ -73,8 +73,31 @@ namespace TestRunner
             Console.WriteLine("Injection successful");
         }
 
+        private static void InjectAssemblyWithExternalFileNeeded()
+        {
+            var assemblyFile = Path.GetFullPath(Path.Combine("..", "..", "..", "WindowsFormHelloWorld", "bin", "Debug", "WindowsFormHelloWorld.exe"));
+            var assemblyExecute = Assembly.LoadFile(assemblyFile);
+
+            var assemblyDir = Path.GetDirectoryName(assemblyExecute.Location);
+            Directory.SetCurrentDirectory(assemblyDir);
+
+            var procInfo = new ProcessStartInfo(assemblyExecute.Location) { WorkingDirectory = assemblyDir };
+            var proc = Process.Start(procInfo);
+            Thread.Sleep(1000);
+
+            var injector = new Injector(proc.Id, typeof(AssemblyWithFileResourceNeeded.Main).Assembly);
+            injector.AddFile("my_file.txt", Encoding.ASCII.GetBytes("External file content"));
+            var injectionResult = injector.Inject();
+            proc.Kill();
+
+            Contract.Assert(injectionResult == InjectionResult.Success);
+            Console.WriteLine("Injection successful");
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Log files will be stored in: {0}", Path.GetFullPath(Path.Combine("..", "..", "..", "WindowsFormHelloWorld", "bin", "Debug")));
+            InjectAssemblyWithExternalFileNeeded();
             InjectDllWithDependency();
             InjectConsoleWithEntryPoint();
             InjectAssemblyWithDefaultInjectMethodSignature();
